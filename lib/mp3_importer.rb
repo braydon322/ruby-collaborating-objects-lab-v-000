25fd32c02d5b47196ce403b1e7f33b3f65b78b0b
Braydon Whitmarsh <braydonwhitmarsh@me.com>
2017-09-11 00:51:17 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -8,9 +8,7 @@ class MP3Importer
   end
 
   def import
-    @@files.each do |file|
-      Song.new_by_filename(file)
-    Song.new_by_filename(song)
+    @files.each {|file| Song.new_by_filename(file)}
   end
   end
 
