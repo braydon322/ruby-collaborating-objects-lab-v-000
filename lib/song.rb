6162ddfb008c037ec77a918fa8b8628dd3e54591
Braydon Whitmarsh <braydonwhitmarsh@me.com>
2017-09-11 00:35:02 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -11,7 +11,9 @@ class Song
   def self.new_by_filename(filename)
     song_string = filename.split(" - ")
     song_name = song_string[1]
+    song_artist = song_string[0]
     new_song = Song.new(song_name)
+    new_song.artist = 
     binding.pry
   end
 
