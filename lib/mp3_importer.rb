928c96fe324f1a5070c05ab7ce13d56c261e57e7
Braydon Whitmarsh <braydonwhitmarsh@me.com>
2017-09-11 00:58:37 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,13 +1,11 @@
 class MP3Importer
-  attr_reader :path, :files, :artist
-
-  def initialize(file_path)
-    @path = file_path
-    @files = Dir.entries(file_path).grep(/.*\.mp3/)
-  end
-
-  def import
-    @files.each {|file| Song.new_by_filename(file)}
-  end
-
-end
+  attr_reader :path, :files
+   def initialize(path)
+     @path = path
+     @files = Dir.entries(path).grep(/.*\.mp3/)
+   end
+ 
+   def import
+     @files.each {|file| Song.new_by_filename(file)}
+   end
+ end
