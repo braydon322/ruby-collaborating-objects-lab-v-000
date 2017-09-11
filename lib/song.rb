28472d25053725cbeca85ef48494fda7b475da46
Braydon Whitmarsh <braydonwhitmarsh@me.com>
2017-09-11 00:54:58 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,26 +6,10 @@ class Song
   end
 
   def self.new_by_filename(filename)
-    song_string = filename.split(" - ")
-    song_name = song_string[1]
-    song_artist = song_string[0]
-
-    exists = nil
-
-    @@songs.each do |song|
-      if song = song_name
-        exists = true
-      else
-        exists = false
-      end
-    end
-
-      if !exists
-        new_song = Song.new(song_name)
-        new_song.artist = song_artist
-        @@songs << self
-      end
-
+    artist, song_name, filetype = filename.split(" - ")
+    new_song = self.new(song_name)
+    artist = Artist.find_or_create_by_name(artist_name)
+    artist.add_song(song)
   end
 
 end
