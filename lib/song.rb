7a85a6e1ebf651822bf12a58e5bcb1f99f17a2f8
Braydon Whitmarsh <braydonwhitmarsh@me.com>
2017-09-11 01:04:34 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,11 +6,18 @@ class Song
   end
 
   def self.new_by_filename(filename)
+    #Splits File Into Array
     songs = filename.split(" - ")
+    #Michael Jackson - Thriller - mp3
+
     song_artist = songs[0]
+    #Michael Jackson
+    
     song_name = songs[1]
+    #Thriller
 
     song = self.new(song_name)
+    binding.pry
     artist = Artist.find_or_create_by_name(song_artist)
     artist.add_song(song)
   end
